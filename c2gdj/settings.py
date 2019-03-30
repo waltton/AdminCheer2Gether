7cd2ac33914fb59f451a363c1637393611fe222a
Waltton Morais <walttonm@gmail.com>
2019-03-30 13:32:39 -0300

    Update allowed hosts

--- c2gdj/settings.py
+++ c2gdj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv('C2G_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
