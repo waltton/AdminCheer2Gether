2e795c6ec7c3ae4791d1a19fcbcefd12df6837f1
Waltton Morais <walttonm@gmail.com>
2019-03-30 20:20:32 -0300

    Improve gm string

--- core/models.py
+++ core/models.py
@@ -89,4 +89,4 @@ class GroupMessage(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return '#{}: {}'.format(self.id, self.user.username)
+        return '#{}: {} => {}'.format(self.id, self.user.username, self.message)
