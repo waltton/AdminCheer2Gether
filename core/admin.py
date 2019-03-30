50b468687447cc216e62b7a70c83aafc90864602
Waltton Morais <walttonm@gmail.com>
2019-03-30 12:30:27 -0300

    Register models on admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from core.models import Tag, User, News, Reaction, UserGroup
+
+
+admin.site.register(Tag)
+admin.site.register(User)
+admin.site.register(News)
+admin.site.register(Reaction)
+admin.site.register(UserGroup)
