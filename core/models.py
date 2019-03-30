cb914556e46235eb147ded93b0b4b49d4fad02a9
Waltton Morais <walttonm@gmail.com>
2019-03-30 12:29:11 -0300

    Add initial models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    title = models.TextField()
+    slug = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class User(models.Model):
+    username = models.TextField()
+    twitter_id = models.TextField()  # tem que ver q dados precisamos do twitter
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class News(models.Model):
+    title = models.TextField()
+    message = models.TextField()
+    media = models.URLField()
+    tags = models.ForeignKey(Tag, on_delete=models.PROTECT)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Reaction(models.Model):
+    news = models.ForeignKey(News, on_delete=models.PROTECT)
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    message = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class UserGroup(models.Model):
+    title = models.TextField()
+    user = models.ManyToManyField(User)
+
+    created_at = models.DateTimeField(auto_now_add=True)
